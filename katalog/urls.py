6e1b9669d3950fd310d2479f9f07eb6f91bfd218
gibranoderenz <gibranoderenz123@gmail.com>
2022-09-11 23:12:16 +0700

    Add catalog feature

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,6 @@
-# TODO: Implement Routings Here
+from django.urls import path
+from katalog import views
+
+urlpatterns = [
+    path("", views.get_catalog_items, name="get_catalog_items")
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import CatalogItem
 
 # TODO: Create your views here.
+
+
+def get_catalog_items(request):
+    catalog_items = CatalogItem.objects.all()
+    context = {
+        "name": "Gibrano Fabien Derenz",
+        "student_ID": "2106750622",
+        "catalog_items": catalog_items
+    }
+    return render(request, "katalog.html", context)
